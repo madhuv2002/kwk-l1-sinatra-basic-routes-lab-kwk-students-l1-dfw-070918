f8243258ad1f04fa4effbf5f6ba48955c5a3d0c9
madhuv2002 <madhuv2002@gmail.com>
2018-07-17 16:42:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   gets '/name' do
     puts "My name is "
   end
+  
+  gets '/hometown' do
+    puts "My hometown is "
+  end
+  
+  gets '/favorite-song' do
+    puts "My favorite-song is "
+  end
 end
