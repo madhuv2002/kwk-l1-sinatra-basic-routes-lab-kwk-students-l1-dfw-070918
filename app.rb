01b6dbea05e111751ccc80bfbea665856ff5316f
madhuv2002 <madhuv2002@gmail.com>
2018-07-17 16:45:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  gets '/name' do
-    "My name is "
+  get '/name' do
+    "My name is"
   end
   
-  gets '/hometown' do
-    "My hometown is "
+  get '/hometown' do
+    "My hometown is"
   end
   
-  gets '/favorite-song' do
-    "My favorite song is "
+  get '/favorite-song' do
+    "My favorite song is"
   end
 end
